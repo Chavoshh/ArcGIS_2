3141cd4391b24f0775044e937eb388e8e1fd6224
Chavosh Almassian <141673571+Chavoshh@users.noreply.github.com>
2025-08-29 07:24:50 +0200

    Update plot_map.py

--- plot_map.py
+++ plot_map.py
@@ -1,1 +1,13 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
+# Load a sample dataset from GeoPandas (world map)
+world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+
+# Filter (example: Europe only)
+europe = world[world["continent"] == "Europe"]
+
+# Plot
+europe.plot(color="lightblue", edgecolor="black")
+plt.title("Simple Map of Europe")
+plt.show()
